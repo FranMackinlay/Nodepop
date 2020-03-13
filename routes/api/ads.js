216ee5f7032a40187d0d7a41a29686243cccc00e
Fran Mackinlay <franmackinlay@gmail.com>
2020-03-13 19:52:27 -0300

    ads routes added

---
+++ routes/api/ads.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const Ad = require('../../models/Ad');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const adName = new RegExp('^' + req.query.adName, "i");
+    const sale = req.query.sale;
+    let price = req.query.price;
+    const tags = req.query.tags;
+    const limit = parseInt(req.query.limit) || 10000;
+    const skip = parseInt(req.query.skip);
+    const sort = req.query.sort;
+
+    const filter = {};
+
+    if (typeof adName !== 'undefined' && adName === new RegExp('^' + req.query.adName, "i")) {
+      filter.adName = adName;
+    }
+    if (typeof sale !== 'undefined') {
+      filter.sale = sale;
+    }
+    if (typeof price !== 'undefined') {
+      if (price.includes('-')) {
+        const priceRange = price.split('-');
+
+        filter.price = { '$gte': priceRange[0], '$lte': priceRange[1] };
+      }
+    }
+
+    if (typeof tags !== 'undefined') {
+      filter.tags = tags;
+    }
+
+    const adList = await Ad.list(filter, limit, skip, sort, tags);
+    console.log(adList);
+
+    if (adList.length === 0) {
+      res.status(404).json({ error: 'Ad not found, try with another query' });
+    }
+
+    res.json(adList);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+
+    const ad = await Ad.findOne({ _id });
+
+    if (!ad) {
+      res.status(404).json({ result: 'Ad not found' });
+    }
+
+    res.json({ result: ad });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const adData = req.body;
+
+  const newAd = new Ad(adData);
+
+  const saveNewAd = await newAd.save();
+
+  res.status(201).json({ result: `Ad created successfully` });
+});
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+
+    const adUpdate = req.body;
+
+    const updatedAd = await Ad.findOneAndUpdate({ _id }, adUpdate, {
+      new: true,
+      useFindAndModify: false
+    });
+
+    res.json({ result: updatedAd });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+
+    await Ad.deleteOne({ _id });
+
+    res.json();
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
