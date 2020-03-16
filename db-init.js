9483e13160bd490c55b9f599b298e15244ff04e9
Fran Mackinlay <franmackinlay@gmail.com>
2020-03-16 13:06:18 -0300

    minor changes

--- db-init.js
+++ db-init.js
@@ -20,29 +20,36 @@ const initAds = async () => {
     {
       adName: "HoverBike",
       sale: true,
-      price: 230.15,
+      price: 230,
       photo: "https://megaricos.com/wp-content/uploads/2018/10/Hoversurf-Hoverbike-1.jpg",
       tags: ["lifestyle", "motor"]
     },
     {
       adName: "Ferrari",
       sale: true,
-      price: 1500.15,
+      price: 1500,
       photo: "https://i.pinimg.com/originals/e4/01/c7/e401c730dfdd8187340198d4a9b39b92.jpg",
       tags: ["lifestyle", "motor", 'work']
     },
     {
       adName: "Motorcycle",
       sale: false,
-      price: 830.15,
+      price: 830,
       photo: "https://i.ytimg.com/vi/ki1UOIqfd10/maxresdefault.jpg",
       tags: ["lifestyle", "motor", "mobile"]
     },
     {
       adName: "Yatch",
       sale: false,
-      price: 2830.15,
+      price: 2830,
       photo: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/23/2016/05/Epiphany-inset-2.jpg",
+      tags: ["lifestyle"]
+    },
+    {
+      adName: "Black Hole",
+      sale: false,
+      price: 150000,
+      photo: "https://www.3dizingof.com/wp-content/uploads/2019/09/nasa-blackhole.gif",
       tags: ["lifestyle"]
     }
   ]);
