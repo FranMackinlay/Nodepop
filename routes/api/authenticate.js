e6d140a4fdf9d0b47b34991df743f0066e69583c
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-03 18:26:18 -0300

    authenticate route created

---
+++ routes/api/authenticate.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const router = express.Router();
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+router.post('/', async (req, res, next) => {
+  try {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const findUser = await User.findOne({ email });
+
+    if (!findUser || !await bcrypt.compare(password, findUser.password)) {
+      const error = new Error('Invalid Credentials');
+      error.status = 401;
+      return next(error);
+    }
+
+    const token = jwt.sign({ _id: findUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '3d',
+    });
+
+    res.json({ token });
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
