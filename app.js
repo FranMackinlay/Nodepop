c617c90c841a62aa769a57c32eb4ef1875913728
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-03 23:38:26 -0300

    i18n implemented

--- app.js
+++ app.js
@@ -13,11 +13,22 @@ require('./lib/connectMongoose');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+  * i18n setup
+*/
+const i18n = require('./lib/i18n-configure')();
+app.use(i18n.init);
+
+/*
+  * Global variable within views
+*/
+app.locals.title = 'Nodepop';
 
 const jwtAuth = require('./lib/jwtAuth');
 
