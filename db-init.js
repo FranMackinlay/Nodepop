4824160a7f23cccbd7abad439e31afe35e265202
Fran Mackinlay <franmackinlay@gmail.com>
2020-06-13 13:44:44 -0300

    static files uploaded

--- db-init.js
+++ db-init.js
@@ -23,35 +23,35 @@ const initAds = async () => {
       adName: "HoverBike",
       sale: true,
       price: 230,
-      photo: "https://cdn.shortpixel.ai/spai/w_1003+h_617+q_lossless+ret_img+to_webp/https://megaricos.com/wp-content/uploads/2018/10/Hoversurf_4.jpg",
+      photo: "/images/hoverbike.jpg",
       tags: ["lifestyle", "motor"]
     },
     {
       adName: "Ferrari",
       sale: true,
       price: 1500,
-      photo: "https://i.pinimg.com/originals/e4/01/c7/e401c730dfdd8187340198d4a9b39b92.jpg",
+      photo: "/images/ferrari.jpg",
       tags: ["lifestyle", "motor", 'work']
     },
     {
       adName: "Motorcycle",
       sale: false,
       price: 830,
-      photo: "https://i.ytimg.com/vi/ki1UOIqfd10/maxresdefault.jpg",
+      photo: "/images/motorcycle.jpg",
       tags: ["lifestyle", "motor", "mobile"]
     },
     {
       adName: "Yatch",
       sale: false,
       price: 2830,
-      photo: "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/23/2016/05/Epiphany-inset-2.jpg",
+      photo: "/images/yatch.jpg",
       tags: ["lifestyle"]
     },
     {
       adName: "Black Hole",
       sale: false,
       price: 150000,
-      photo: "https://www.3dizingof.com/wp-content/uploads/2019/09/nasa-blackhole.gif",
+      photo: "/images/blackhole.gif",
       tags: ["lifestyle"]
     }
   ]);
